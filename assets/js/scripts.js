10f3d2b7fe19dc70e6d86b03dce77ee7cd548426
ProgrammerXD <70265763+naman77s@users.noreply.github.com>
2022-09-20 22:54:31 +0530

    Add files via upload

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,38 +9,50 @@ $.get("footer.html", function(data){
 $.get("head.html", function(data){
     $("#head-placeholder").replaceWith(data);
 });
-$.get('head.html', function (response) {
-        $('#head-placeholder').html(response);
-    });
 console.log("//All files loaded succesfully...");
 
+//Theme Toggle
+function changeTheme(){
+   const togle = document.getElementById("toggle");
+   const thm = document.getElementById('theme-re');
+   const themeA = document.getElementById("theme");
+   const themeB = document.getElementById("themeR");
+   const themeC = document.getElementById("themeH");
+   if (togle.checked == true){
+		themeA.setAttribute("href" , './assets/css/dark.css');
+	    themeB.setAttribute("href" , './assets/css/responsive-dark.css');
+	    themeC.setAttribute("content" , '#fff');
+	    thm.setAttrubute("href" , '');
+	    console.log('55');
+   }
+   else{
+		themeA.setAttribute("href" , './assets/css/style.css');
+		thm.setAttrubute("href" , './assets/css/style.css');
+	    themeB.setAttribute("href" , './assets/css/responsive.css');
+	    themeC.setAttribute("content" , '#000');
+	    console.log('77');
+   }
+}
+//MenuBar Toggle
+var sn = document.getElementById("Snav");
+function dump(){
+	sn.style.left = "-120%";
+}
+function pull(){
+	sn.style.left = "0";
+}
+
+
 //Useless Functions
+/*
 document.body.addEventListener("scroll", function(event) {
   var opacity = (document.body.offsetHeight - document.body.scrollTop) / document.body.offsetHeight;
   var scale = (document.body.offsetHeight - document.body.scrollTop) / document.body.offsetHeight;
   document.documentElement.style.setProperty('--headerOpacity', opacity);
   document.documentElement.style.setProperty('--headerScale', scale);
 });
-
-//Theme Toggle
-function changeTheme(){
-   const togle = document.getElementById("toggle");
-   const themeA = document.getElementById("theme");
-   const themeB = document.getElementById("themeR");
-   const themeC = document.getElementById("themeH");
-   if (togle.checked == true){
-		themeA.setAttribute("href" , './assets/css/dark.css');
-    themeB.setAttribute("href" , './assets/css/responsive-dark.css');
-    themeC.setAttribute("content" , '#fff');
-   }
-   else{
-		themeA.setAttribute("href" , './assets/css/style.css');
-    themeB.setAttribute("href" , './assets/css/responsive.css');
-    themeC.setAttribute("content" , '#000');
-   }
-}
-
 //Scroll Functions
+/*
   $("a").on('click', function(event) {
     if (this.hash !== "") {
       event.preventDefault();
@@ -52,12 +64,4 @@ function changeTheme(){
       });
     }
   });
-
-//MenuBar Toggle
-var sn = document.getElementById("Snav");
-function dump(){
-	sn.style.left = "-120%";
-}
-function pull(){
-	sn.style.left = "0";
-}
+*/
