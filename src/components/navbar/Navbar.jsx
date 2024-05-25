583404ada60df00856516b2568e6e7eb3b213ae5
Naman Saini <thisisnamansaini@gmail.com>
2024-05-25 19:56:40 +0530

    fix[navbar] 🐛 eslint warning

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import { NavLink, useLocation } from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Sidebar from '../sidebar/Sidebar';
 import { HiMenu } from "react-icons/hi";
 
@@ -13,13 +13,14 @@ const Navbar = () => {
     const openSidebar = () => {
         setSidebarVisible(true);
     };
-    const closeSidebar = () => {
+
+    const closeSidebar = useCallback(() => {
         setSidebarVisible(false);
-    }
+    }, []);
 
     useEffect(() => {
         setSidebarVisible(false);
-    }, [])
+    }, []);
 
     useEffect(() => {
         const handleOutsideClick = (event) => {
@@ -35,19 +36,17 @@ const Navbar = () => {
         };
     }, [closeSidebar]);
 
-
     function ToastDisplay() {
         return (
             <div className='m-2'>
                 <p className='text-md'>Coming Soon !</p>
             </div>
-        )
-    };
+        );
+    }
 
     return (
         <>
             <nav className='fixed z-50 w-screen h-[6vh] flex justify-between py-1 px-3 lg:px-7 shadow-md bg-black text-white'>
-
                 {/* Logo */}
                 <NavLink to="/" className='flex justify-center w-fit lg:w-auto items-center gap-3 bg-gray-800 rounded-full px-3 pr-4'>
                     <img className='w-[5vw] lg:w-[2vw] animate-pulse' src="/images/logos/scaledlogo.png" alt="ufolinux logo" />
@@ -55,53 +54,30 @@ const Navbar = () => {
                 </NavLink>
 
                 {/* Navlinks */}
-                <div className="font-semibold text-lg px-3 text-center hidden lg:flex justify-center items-center" >
+                <div className="font-semibold text-lg px-3 text-center hidden lg:flex justify-center items-center">
                     <ul className='w-full flex justify-center items-center gap-7 *:text-center'>
                         <li className={location.pathname === "/" ? 'text-red-400 underline underline-offset-8' : ''}>
-                            <NavLink to="/" >
-                                Home
-                            </NavLink>
+                            <NavLink to="/">Home</NavLink>
                         </li>
-
                         <li onClick={() => toast(<ToastDisplay className='bg-neutral-700 m-2' />)}>
-                            {/* <NavLink to="/docs" > */}
                             Docs
-                            {/* </NavLink> */}
                         </li>
-
                         <li onClick={() => toast(<ToastDisplay className='bg-neutral-700 m-2' />)}>
-                            {/* <NavLink to="/blogs" > */}
                             Blogs
-                            {/* </NavLink> */}
                         </li>
-
                         <li className={location.pathname === "/contributors" ? 'text-red-400 underline underline-offset-8' : ''}>
-                            <NavLink to="/contributors" >
-                                Contributors
-                            </NavLink>
+                            <NavLink to="/contributors">Contributors</NavLink>
                         </li>
-
                         <li className={location.pathname === "/download" ? 'text-red-400 underline underline-offset-8' : ''}>
-                            <NavLink to="/download" >
-                                Download
-                            </NavLink>
+                            <NavLink to="/download">Download</NavLink>
                         </li>
-
                         <li className={location.pathname === "/mirror" ? 'text-red-400 underline underline-offset-8' : ''}>
-                            <NavLink to="/mirror" >
-                                Mirror list
-                            </NavLink>
+                            <NavLink to="/mirror">Mirror list</NavLink>
                         </li>
-
-                        {/* <li className={location.pathname === "#contact" ? 'text-red-400 underline underline-offset-8' : ''}>
-                            <a href="#contact" >
-                                Contact Us
-                            </a>
-                        </li> */}
                     </ul>
                 </div>
 
-                {/* menu bar */}
+                {/* Menu bar */}
                 <button className='text-white block lg:hidden' onClick={openSidebar}><HiMenu size="30px" /></button>
 
                 {/* Sidebar */}
@@ -122,7 +98,7 @@ const Navbar = () => {
                 pauseOnHover={!sidebarVisible}
                 theme="dark" />
         </>
-    )
+    );
 }
 
 export default Navbar;
